98cc6f89cf07d87d19a277c45de84e7e57871b8b
Dacs Dacallos <dacsstack@gmail.com>
2024-08-06 20:42:52 -0800

    Uppdated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -58,7 +58,7 @@ export const Footer = () => {
             rel="noreferrer"
             className="hover:underline"
           >
-            Git/Github
+            Github
           </a>
         </li>
       </ul>
