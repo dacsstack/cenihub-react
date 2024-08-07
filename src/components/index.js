53373d60cb0883711ff31851dc90db077690398a
Dacs Dacallos <dacsstack@gmail.com>
2024-08-06 18:55:27 -0800

    Deployment

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { Footer } from "./Footer";
+export { Header } from "./Header";
+
+export { Button } from "./Button";
+export { Card } from "./Card";
+
+export { ScrollToTop } from "./ScrollToTop";
